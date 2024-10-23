77b627263b0a6d6eaefc2a4d1e99df40bd70d433
Kieran Labarrere <web2020.kieran.labarrere@gmail.com>
2024-10-23 15:50:46 +0200

    event_view.php, tout rempli d'après json brut

--- src/view/event_view.php
+++ src/view/event_view.php
@@ -4,7 +4,6 @@
         $this->eventDatas=$eventModel->event;
     } 
     public function getEventView($lang){
-    
         $eventDatas=$this->eventDatas;
         $id=$eventDatas->id;
         $date=$eventDatas->time->date;
@@ -29,6 +28,51 @@
                 </div>
                 <h3 data-field="title">$title<h3>
                 <div class="activity-illustration"><img src="/public/img/content/events/$illustration" data-field="illustration"></div>
+            <div class="activity-types" data-field="types">
+                <span class="card-type">exposition</span>
+                <span class="card-type">Repas</span>
+                <span class="card-type">concert</span>
+            </div>
+            <article class="activity-description">
+            <div class="activity-texte">
+                    <p data-field="description">
+                    $description
+                    </p>
+                </div>
+            </article>
+                        <article>
+                <h3>Infos pratiques</h3>
+                <section class="infospratik">
+                    <article class="infos-elements">
+                        <span data-field="organisateur">Organisateur : <a href="https://www.bathyscaphe.be/">Bathyscaphe.be</a></span>
+                        <hr>
+                        <div>
+                            <span class="infosdates" data-field="dates">$date<br></span>
+                            <span class="infosHoraires" data-field="horaire">$horaire</span>
+                        </div>
+                        <hr>
+                        <div class="infosresa">
+                            Réservation obligatoire: <b><span data-field="booking">$necessitedbook</span></b><br>
+                            <span>Prix:<span data-field="price">18</span>$price</span>
+                        </div>
+                        <hr>
+                        <span class="infoslocation" data-field="location">Rue Dieudonné Lefèvre 215<br>Bruxelles<br>Belgique</span>
+                    </article>
+                    <div id="map2" class="infos-map-activity"></div>
+                <script src="./js/share.js"></script>
+                <script src="./js/mapbathy.js"></script>
+                </section>
+            </article>
+            <br>
+            <hr>
+            </section>            <article>
+                <h3>Billets</h3>
+                <section>
+                <a href="$ticketlink" onclick="var w=window.open('$ticketlink', 'Reserver', 'width=650, height=600, top=100, left=100, toolbar=no, resizable=yes, scrollbars=yes, status=no'); w.focus(); return false;"><img style="width:200px;" src="https://www.billetweb.fr/images/buttons/billetterie_bleu.png"></a>
+                </section>
+            </article>
+        </article>
+    </section>
                 
 
 EVENTVIEWHTML;
