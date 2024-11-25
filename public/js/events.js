cff050cf24d27f95de7ab454192dc516aed90e5b
Kieran Labarrere <web2020.kieran.labarrere@gmail.com>
2024-11-25 12:12:55 +0100

    public favicon.ico; js events-toggle action

--- public/js/events.js
+++ public/js/events.js
@@ -1,9 +1,17 @@
-// Gestion des sidebars
-document.querySelector(".btn-past").addEventListener("click", function() {
-    document.querySelector(".past-sidebar").classList.toggle("show");
-  });
-  
-  document.querySelector(".btn-future").addEventListener("click", function() {
-    document.querySelector(".future-sidebar").classList.toggle("show");
-  });
+function toggleAside(side) {
+  // Sélectionne l'aside en fonction du côté (past ou future)
+  const aside = document.querySelector(`.${side}-sidebar`);
+
+  // Bascule la classe "show" pour afficher ou masquer l'aside
+  aside.classList.toggle("show");
+}
+
+// Associer les fonctions aux boutons
+document.querySelector(".time-button:not(.incoming)").addEventListener("click", function() {
+  toggleAside("past-sidebar");
+});
+
+document.querySelector(".time-button.incoming").addEventListener("click", function() {
+  toggleAside("future-sidebar");
+});
   
