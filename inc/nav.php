3c2490ea65cb7f27e433a4f7e15492c259247158
Kieran Labarrere <web2020.kieran.labarrere@gmail.com>
2025-02-25 00:54:29 +0100

    menu.css, nav.php, menu.js, on a un menu responsive correct en conne, une icone qui fonctionne

--- inc/nav.php
+++ inc/nav.php
@@ -1,14 +1,15 @@
 <?php
- $menuMain_model=$menus->getMenu("Main_menu");
- require_once("../src/view/view_menus.php");
- $menusView=new ViewMenu($lang);
- $menuMain_view=$menusView->getViewMainMenu($menuMain_model,$singlePage);
- ?>
+$menuMain_model = $menus->getMenu("Main_menu");
+require_once("../src/view/view_menus.php");
+$menusView = new ViewMenu($lang);
+$menuMain_view = $menusView->getViewMainMenu($menuMain_model, $singlePage);
+?>
 <nav class="responsiveMenu" id="responsiveMenu">
-        <?php
-        echo $menuMain_view;
-        ?>
-        <a href="javascript:void(0);" class="icon" onclick="responsiveMenu()">
-            <img src="img/deco/menu-responsive-btn-blanc.svg" alt="bouton menu-toggle">
-        </a>
+<a href="javascript:void(0);" class="icon" onclick="responsiveMenu()">
+        <span id="menuIcon">☰</span> <!-- Icône du menu -->
+    </a>
+    <?php
+    echo $menuMain_view;
+    ?>
+
 </nav>
