894599dfb20148d55b4c9062b25e0380fedd909f
Kieran Labarrere <web2020.kieran.labarrere@gmail.com>
2025-02-25 04:51:49 +0100

    footer.php, footer.css, img deco, bathyscaphe dans footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,9 +1,10 @@
 <footer>
+
             <div class="footerNav">
 
                 <nav class="navfooterbloc">
                     <h2>Contacts</h2>
-                    <a class="maillink" href="info@annebsollis.com">info@bathyscaphe.be</a>
+                    <a class="maillink" href="info@bathyscaphe.be">info@bathyscaphe.be</a>
                     <a class="phonelink" href="tel:+32485966694">+32(0)485 96 66 94</a>
                     <address class="situationlink">rue Dieudonné Lefèvre, 215<br> 1020 Brussels - Belgium</address>
                 </nav>
@@ -12,6 +13,8 @@
                     <?php echo $menuMain_view?>
                 </nav>
             </div>
+            <img class="footer-logo" src="<?=$repImgDeco?>logo.png" alt="">
+
     <nav id="menuRS" class="nav-rs">
         <?php 
                     foreach($menuRS as $item){
