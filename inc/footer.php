ae07bea4ed2e97e35080a7dc50373775e1dd9569
Kieran Labarrere <web2020.kieran.labarrere@gmail.com>
2025-02-25 03:08:44 +0100

    footer.css, Invertion des blocs de menus

--- inc/footer.php
+++ inc/footer.php
@@ -1,14 +1,15 @@
 <footer>
             <div class="footerNav">
-                <nav class="navfooterbloc">
-                    <h2>Menu</h2>
-                    <?php echo $menuMain_view?>
-                </nav>
+
                 <nav class="navfooterbloc">
                     <h2>Contacts</h2>
                     <a class="maillink" href="info@annebsollis.com">info@bathyscaphe.be</a>
                     <a class="phonelink" href="tel:+32485966694">+32(0)485 96 66 94</a>
                     <address class="situationlink">rue Dieudonné Lefèvre, 215<br> 1020 Brussels - Belgium</address>
+                </nav>
+                <nav class="navfooterbloc">
+                    <h2>Menu</h2>
+                    <?php echo $menuMain_view?>
                 </nav>
             </div>
     <nav id="menuRS" class="nav-rs">
